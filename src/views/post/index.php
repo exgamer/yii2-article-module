87bfd5a9e6cc214fd1616f08470f292bfa19ac70
citizenzet <exgamer@live.ru>
2019-12-05 18:00:12 +0600

    d

--- src/views/post/index.php
+++ src/views/post/index.php
@@ -111,7 +111,7 @@ $this->viewHelper()->pushPageHeader();
 
                     return Html::a(
                         '<i class="icon-checkmark4"></i>'. Yii::t('yii2admin', 'Активировать'),
-                        ['status-change', 'id' => $model['id'], 'status' => StatusEnum::ACTIVE],
+                        ['status-change', 'id' => $model['id'], 'status' => StatusEnum::ACTIVE, 'locale' => $model['locale']],
                         [
                             'class' => 'admin-action dropdown-item',
                             'data-pjax-id' => 'list-pjax',
@@ -129,7 +129,7 @@ $this->viewHelper()->pushPageHeader();
                     }
                     return Html::a(
                         '<i class="icon-cross2"></i>'. Yii::t('yii2admin', 'Деактивировать'),
-                        ['status-change', 'id' => $model['id'], 'status' => StatusEnum::INACTIVE],
+                        ['status-change', 'id' => $model['id'], 'status' => StatusEnum::INACTIVE, 'locale' => $model['locale']],
                         [
                             'class' => 'admin-action dropdown-item',
                             'data-pjax-id' => 'list-pjax',
@@ -145,7 +145,7 @@ $this->viewHelper()->pushPageHeader();
 
                     return Html::a(
                         '<i class="icon-trash"></i>'. Yii::t('yii2admin', 'Удалить'),
-                        ['delete', 'id' => $model['id']],
+                        ['delete', 'id' => $model['id'], 'locale' => $model['locale']],
                         [
                             'class' => 'admin-action dropdown-item',
                             'data-pjax-id' => 'list-pjax',
