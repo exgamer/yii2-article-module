96807b5064df971664a431bafb6b6a545f1cc380
Olzhas Kulzhambekov <exgamer@live.ru>
2019-12-07 19:15:29 +0600

    а

--- src/console/migrations/m191002_151217_post_category_table_localization.php
+++ src/console/migrations/m191002_151217_post_category_table_localization.php
@@ -15,7 +15,7 @@ class m191002_151217_post_category_table_localization extends Migration
     public function up()
     {
         $this->addTable([
-            'id' => $this->bigPrimaryKey(),
+//            'id' => $this->bigPrimaryKey(),
             'entity_id' => $this->bigInteger()->notNull(),
             'locale' => $this->bigInteger()->notNull(),
             'seo_name' => $this->string(1024),
@@ -28,8 +28,9 @@ class m191002_151217_post_category_table_localization extends Migration
             'title' => $this->string(1024)->notNull(),
             'content' => $this->text()->notNull()
         ]);
+        $this->addPK(['entity_id', 'locale'], true);
         $this->addIndex(['entity_id']);
-        $this->addIndex(['entity_id', 'locale'], true);
+//        $this->addIndex(['entity_id', 'locale'], true);
         $this->addIndex(['locale']);
         $this->addIndex(['url']);
         $this->execute("ALTER TABLE post_category_localization
