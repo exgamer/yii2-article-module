cbd4ed95b9da1e7d1ffc080ad1e0ad0290174b6a
i.skiba <ilya@mediabet.kz>
2020-03-20 05:46:27 +0000

    fix: post form

--- src/views/post/_form.php
+++ src/views/post/_form.php
@@ -25,88 +25,71 @@ $saveButton = Html::submitButton(
 );
 ?>
 
-<?php Pjax::begin(['formSelector' => '#static-page-form']); ?>
-<?php if (Yii::$app->localeService->catalogCount() > 1): ?>
-    <ul class="nav nav-tabs nav-tabs-solid nav-justified bg-light">
-        <?php foreach (Yii::$app->localeService->catalog() as $key => $locale):?>
-            <li class="nav-item">
-                <?= Html::a(
-                    $locale,
-                    \yii\helpers\Url::current(['locale' => $key]),
-                    ['class' => 'nav-link ' . ($key ==  $model->locale   ? "active" : "")]
-                ) ?>
-            </li>
-        <?php endforeach;?>
-    </ul>
-<?php endif; ?>
-<?php $form = ActiveForm::begin(['id' => 'static-page-form']); ?>
-<div class="card">
-    <div class="card-body text-right">
-        <?= $saveRedirectButton?>
-        <?= $saveButton?>
+<?php Pjax::begin(['formSelector' => '#post-form']); ?>
+    <?php if (Yii::$app->localeService->catalogCount() > 1): ?>
+        <ul class="nav nav-tabs nav-tabs-solid nav-justified bg-light">
+            <?php foreach (Yii::$app->localeService->catalog() as $key => $locale):?>
+                <li class="nav-item">
+                    <?= Html::a(
+                        $locale,
+                        \yii\helpers\Url::current(['locale' => $key]),
+                        ['class' => 'nav-link ' . ($key ==  $model->locale   ? "active" : "")]
+                    ) ?>
+                </li>
+            <?php endforeach;?>
+        </ul>
+    <?php endif; ?>
+    <?php
+        $form = ActiveForm::begin(['id' => 'post-form', 'model' => $originModel ?? new Post()]);
+
+        echo Html::activeHiddenInput($model, 'locale');
+    ?>
+    <div class="card">
+        <div class="card-body text-right">
+            <?= $saveRedirectButton?>
+            <?= $saveButton?>
+        </div>
     </div>
-</div>
 
-<div class="card">
-    <div class="card-body">
-        <legend class="font-weight-semibold text-uppercase font-size-sm">
-            <?= Yii::t('yii2admin', 'Основные данные') ;?>
-        </legend>
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12">
-                <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12">
-                <?= $form->field($model, 'anons')->textarea();?>
-            </div>
-        </div>
-
-        <!--            ===========category select start====-->
-
-
-        <?php if (empty($model->categoryParents)) :?>
+    <div class="card">
+        <div class="card-body">
+            <legend class="font-weight-semibold text-uppercase font-size-sm">
+                <?= Yii::t('yii2admin', 'Основные данные') ;?>
+            </legend>
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12">
-                    <?= $form
-                        ->field($model, 'category_id')
-                        ->dropDownList(Yii::$app->postCategoryService->getDropDownList(), [
-                            'class' => 'form-control  form-control-uniform active-form-refresh-control',
-                            'prompt' => Yii::t('yii2admin', 'Выберите категорию')
-                        ]);
-                    ?>
+                    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
                 </div>
             </div>
-        <?php endif;?>
-
-        <?php if (! empty($model->categoryParents)) :?>
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12">
-                    <div class="form-group">
-                        <?= Html::label(Yii::t('yii2admin', 'Основная категория'))?>
-                        <?= Html::dropDownList('category_root', reset($model->categoryParents), Yii::$app->postCategoryService->getDropDownList(), [
-                            'class' => 'form-control  form-control-uniform active-form-refresh-control',
-                            'prompt' => Yii::t('yii2admin', 'Выберите категорию')
-                        ]);?>
-                        <div class="text-danger form-text"></div>
+                    <?= $form->field($model, 'anons')->textarea();?>
+                </div>
+            </div>
+
+            <!--            ===========category select start====-->
+
+
+            <?php if (empty($model->categoryParents)) :?>
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <?= $form
+                            ->field($model, 'category_id')
+                            ->dropDownList(Yii::$app->postCategoryService->getDropDownList(), [
+                                'class' => 'form-control  form-control-uniform active-form-refresh-control',
+                                'prompt' => Yii::t('yii2admin', 'Выберите категорию')
+                            ]);
+                        ?>
                     </div>
                 </div>
-            </div>
-            <?php
-            $count = count($model->categoryParents);
-            $i = 0;
-            ?>
-            <?php foreach ($model->categoryParents as $key => $parentId) : ?>
-                <?php
-                if ($count-1 == $i){
-                    continue;
-                }
-                ?>
+            <?php endif;?>
+
+            <?php if (! empty($model->categoryParents)) :?>
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-sm-12">
                         <div class="form-group">
-                            <?= Html::dropDownList('category'.$parentId, $model->categoryParents[$key +1], Yii::$app->postCategoryService->getChildsDropDownList($parentId), [
+                            <?= Html::label(Yii::t('yii2admin', 'Основная категория'))?>
+                            <?= Html::dropDownList('category_root', reset($model->categoryParents), Yii::$app->postCategoryService->getDropDownList(), [
                                 'class' => 'form-control  form-control-uniform active-form-refresh-control',
                                 'prompt' => Yii::t('yii2admin', 'Выберите категорию')
                             ]);?>
@@ -114,122 +97,143 @@ $saveButton = Html::submitButton(
                         </div>
                     </div>
                 </div>
-                <?php $i++; ?>
-            <? endforeach;?>
+                <?php
+                $count = count($model->categoryParents);
+                $i = 0;
+                ?>
+                <?php foreach ($model->categoryParents as $key => $parentId) : ?>
+                    <?php
+                    if ($count-1 == $i){
+                        continue;
+                    }
+                    ?>
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12">
+                            <div class="form-group">
+                                <?= Html::dropDownList('category'.$parentId, $model->categoryParents[$key +1], Yii::$app->postCategoryService->getChildsDropDownList($parentId), [
+                                    'class' => 'form-control  form-control-uniform active-form-refresh-control',
+                                    'prompt' => Yii::t('yii2admin', 'Выберите категорию')
+                                ]);?>
+                                <div class="text-danger form-text"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php $i++; ?>
+                <? endforeach;?>
+
+
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <?= $form
+                            ->field($model, 'category_id')
+                            ->dropDownList(Yii::$app->postCategoryService->getChildsDropDownList(array_pop($model->categoryParents)), [
+                                'class' => 'form-control  form-control-uniform active-form-refresh-control',
+                                'prompt' => Yii::t('yii2admin', 'Выберите категорию')
+                            ]);
+                        ?>
+                    </div>
+                </div>
+            <?php endif;?>
+
+
+            <!--            ===========category select end====-->
+
 
 
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12">
-                    <?= $form
-                        ->field($model, 'category_id')
-                        ->dropDownList(Yii::$app->postCategoryService->getChildsDropDownList(array_pop($model->categoryParents)), [
-                            'class' => 'form-control  form-control-uniform active-form-refresh-control',
-                            'prompt' => Yii::t('yii2admin', 'Выберите категорию')
-                        ]);
-                    ?>
+                    <?= $this->render('/include/_editor.php', [
+                        'form' => $form,
+                        'model' => $model,
+                        'attribute' => 'content',
+                        'originModel' => isset($originModel) ? $originModel : null
+                    ]) ?>
                 </div>
             </div>
-        <?php endif;?>
 
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <?= $this->render('/include/_uploader.php', [
+                        'form' => $form,
+                        'model' => $model,
+                        'attribute' => 'image',
+                        'originModel' => isset($originModel) ? $originModel : null
+                    ]) ?>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <?= $this->render('/include/_uploader.php', [
+                        'form' => $form,
+                        'model' => $model,
+                        'attribute' => 'image_anons_big',
+                        'originModel' => isset($originModel) ? $originModel : null
+                    ]) ?>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <?= $this->render('/include/_uploader.php', [
+                        'form' => $form,
+                        'model' => $model,
+                        'attribute' => 'image_anons',
+                        'originModel' => isset($originModel) ? $originModel : null
+                    ]) ?>
+                </div>
+            </div>
 
-        <!--            ===========category select end====-->
-
-
-
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12">
-                <?= $this->render('/include/_editor.php', [
-                    'form' => $form,
-                    'model' => $model,
-                    'attribute' => 'content',
-                    'originModel' => isset($originModel) ? $originModel : null
-                ]) ?>
+            <legend class="font-weight-semibold text-uppercase font-size-sm">
+                <?= Yii::t('yii2admin', 'SEO настройки') ;?>
+            </legend>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <?= $form->field($model, 'seo_name')->textInput(['maxlength' => true, 'disabled' => isset($originModel) ? true : false]) ?>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <?= $form->field($model, 'seo_h1')->textInput(['maxlength' => true]) ?>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <?= $form->field($model, 'seo_title')->textInput(['maxlength' => true]) ?>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <?= $form->field($model, 'seo_description')->textInput(['maxlength' => true]) ?>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <?= $form->field($model, 'seo_keywords')->textInput(['maxlength' => true]) ?>
+                </div>
             </div>
-        </div>
-
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12">
-                <?= $this->render('/include/_uploader.php', [
-                    'form' => $form,
-                    'model' => $model,
-                    'attribute' => 'image',
-                    'originModel' => isset($originModel) ? $originModel : null
-                ]) ?>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-                <?= $this->render('/include/_uploader.php', [
-                    'form' => $form,
-                    'model' => $model,
-                    'attribute' => 'image_anons_big',
-                    'originModel' => isset($originModel) ? $originModel : null
-                ]) ?>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-                <?= $this->render('/include/_uploader.php', [
-                    'form' => $form,
-                    'model' => $model,
-                    'attribute' => 'image_anons',
-                    'originModel' => isset($originModel) ? $originModel : null
-                ]) ?>
-            </div>
-        </div>
-
-        <legend class="font-weight-semibold text-uppercase font-size-sm">
-            <?= Yii::t('yii2admin', 'SEO настройки') ;?>
-        </legend>
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12">
-                <?= $form->field($model, 'seo_name')->textInput(['maxlength' => true, 'disabled' => isset($originModel) ? true : false]) ?>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-                <?= $form->field($model, 'seo_h1')->textInput(['maxlength' => true]) ?>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-                <?= $form->field($model, 'seo_title')->textInput(['maxlength' => true]) ?>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-                <?= $form->field($model, 'seo_description')->textInput(['maxlength' => true]) ?>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-                <?= $form->field($model, 'seo_keywords')->textInput(['maxlength' => true]) ?>
-            </div>
-        </div>
-        <legend class="font-weight-semibold text-uppercase font-size-sm">
-            <?= Yii::t('yii2admin', 'Дополнительно') ;?>
-        </legend>
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12">
-                <?= $form
-                    ->field($model, 'status', [
-                        'template' => '
-                                            <div class="form-check form-check-inline mt-2">
-                                                {input}
-                                            </div>
-                                            {error}
-                                        '
-                    ])
-                    ->checkbox(
-                        [
-                            'label' => Yii::t('yii2admin', 'Активировано'),
-                            'class' => 'form-check-input-styled-primary',
-                            'labelOptions' => ['class' => 'form-check-label control-label']
-                        ],
-                        true
-                    )
-                ?>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-                <?= $form->field($model, 'sort')->textInput(['maxlength' => true]) ?>
+            <legend class="font-weight-semibold text-uppercase font-size-sm">
+                <?= Yii::t('yii2admin', 'Дополнительно') ;?>
+            </legend>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <?= $form
+                        ->field($model, 'status', [
+                            'template' => '
+                                                <div class="form-check form-check-inline mt-2">
+                                                    {input}
+                                                </div>
+                                                {error}
+                                            '
+                        ])
+                        ->checkbox(
+                            [
+                                'label' => Yii::t('yii2admin', 'Активировано'),
+                                'class' => 'form-check-input-styled-primary',
+                                'labelOptions' => ['class' => 'form-check-label control-label']
+                            ],
+                            true
+                        )
+                    ?>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <?= $form->field($model, 'sort')->textInput(['maxlength' => true]) ?>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="card">
-    <div class="card-body text-right">
-        <?= $saveRedirectButton?>
-        <?= $saveButton?>
+    <div class="card">
+        <div class="card-body text-right">
+            <?= $saveRedirectButton?>
+            <?= $saveButton?>
+        </div>
     </div>
-</div>
-<?php ActiveForm::end(); ?>
+    <?php ActiveForm::end(); ?>
 <?php Pjax::end(); ?>
