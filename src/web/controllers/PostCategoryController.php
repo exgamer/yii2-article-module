f64a15f55e6f0836211155656ef47ddd0536c780
Olzhas Kulzhambekov <exgamer@live.ru>
2020-05-20 15:45:14 +0600

    rbac

--- src/web/controllers/PostCategoryController.php
+++ src/web/controllers/PostCategoryController.php
@@ -2,9 +2,13 @@
 
 namespace concepture\yii2article\web\controllers;
 
+use concepture\yii2handbook\actions\PositionSortIndexAction;
+use concepture\yii2handbook\services\EntityTypePositionSortService;
 use concepture\yii2user\enum\UserRoleEnum;
 use concepture\yii2logic\actions\web\localized\tree\StatusChangeAction;
 use concepture\yii2logic\actions\web\localized\tree\UndeleteAction;
+use kamaelkz\yii2admin\v1\actions\EditableColumnAction;
+use kamaelkz\yii2admin\v1\actions\SortAction;
 use kamaelkz\yii2cdnuploader\actions\web\ImageDeleteAction;
 use kamaelkz\yii2cdnuploader\actions\web\ImageUploadAction;
 use yii\helpers\ArrayHelper;
@@ -29,6 +33,23 @@ class PostCategoryController extends Controller
             'undelete' => UndeleteAction::class,
             'image-upload' => ImageUploadAction::class,
             'image-delete' => ImageDeleteAction::class,
+            PositionSortIndexAction::actionName() => [
+                'class' => PositionSortIndexAction::class,
+                'entityColumns' => [
+                    'id',
+                    'header',
+                    'seo_name',
+                ],
+                'labelColumn' => 'header',
+            ],
+            EditableColumnAction::actionName() => [
+                'class' => EditableColumnAction::class,
+                'serviceClass' => EntityTypePositionSortService::class
+            ],
+            SortAction::actionName() => [
+                'class' => SortAction::class,
+                'serviceClass' => EntityTypePositionSortService::class
+            ],
         ]);
     }
 }
